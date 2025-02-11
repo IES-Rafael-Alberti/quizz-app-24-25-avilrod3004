dff8783f805a67fdee788c3710de6626e415b51f
avilrod3004 <avilrod3004@g.educaand.es>
2025-02-11 18:29:40 +0100

    estructura header en la pagina index

--- src/index.php
+++ src/index.php
@@ -5,16 +5,18 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/variables.css">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
     <title>Quizzes App</title>
 </head>
 <body>
-    <header>
-        <h1>Quizzes App</h1>
+    <header class="encabezado">
+        <h1 class="encabezado__titulo">Quizzes App</h1>
 
-        <nav>
-            <ul>
-                <li><a href="user/login.php">Login</a></li>
-                <li><a href="user/register.php">Registro</a></li>
+        <nav class="encabezado__navegacion">
+            <ul class="navegacion__listado">
+                <li class="listado__opcion"><a href="user/login.php" class="opcion__enlace">Login</a></li>
+                <li class="listado__opcion"><a href="user/register.php" class="opcion__enlace">Registro</a></li>
             </ul>
         </nav>
     </header>
