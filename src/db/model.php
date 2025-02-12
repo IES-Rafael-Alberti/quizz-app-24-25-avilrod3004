f44f869322cd8f3e051fb2f1163a676330bf63d4
avilrod3004 <avilrod3004@g.educaand.es>
2025-02-12 22:17:15 +0100

    funciones crud para la tabla question

--- src/db/model.php
+++ src/db/model.php
@@ -38,13 +38,13 @@ class Model {
      * @param $description - Descripción
      * @return int Número de filas afeactadas, 1
      */
-    public function crear_quiz($title, $description, $owner) {
+    public function crear_quiz($title, $description, $owner): int {
         $statement = $this->conn->prepare("INSERT INTO quiz (title, description, owner) VALUES (:title, :description, :owner)");
         $statement->execute(array(":title" => $title, ":description" => $description, ":owner" => $owner));
         return $statement->rowCount();
     }
 
-    public function obtener_quizzes_instructor($id_instructor) {
+    public function obtener_quizzes_instructor($id_instructor): array {
         $statement = $this->conn->prepare("SELECT * FROM quiz WHERE owner = :id_instructor");
         $statement->execute(array(":id_instructor" => $id_instructor));
         return $statement->fetchAll(PDO::FETCH_CLASS, "Quiz", array("follow" => true));
@@ -56,7 +56,7 @@ class Model {
         return $statement->fetchObject("Quiz");
     }
 
-    public function update_quiz($quiz_id, $title, $description, $owner) {
+    public function update_quiz($quiz_id, $title, $description, $owner): int {
         $statement = $this->conn->prepare("UPDATE quiz SET title = :title, description = :description, owner = :owner WHERE quiz_id = :quiz_id");
         $statement->execute(array(":title" => $title, ":description" => $description, ":owner" => $owner, ":quiz_id" => $quiz_id));
         return $statement->rowCount();
@@ -65,6 +65,40 @@ class Model {
     public function borrar_quiz($quiz_id) {
         $statement = $this->conn->prepare("DELETE FROM quiz WHERE quiz_id = :quiz_id");
         $statement->execute(array(":quiz_id" => $quiz_id));
+        return $statement->rowCount();
+    }
+
+
+    // TABLA DE PREGUNTAS - QUESTION
+
+    public function crear_pregunta($quiz_id, $question_text, $option_a, $option_b, $option_c, $option_d, $correct_option): int {
+        $statement = $this->conn->prepare("INSERT INTO question (quiz_id, question_text, option_a, option_b, option_c, option_d, correct_option) 
+                                                VALUES (:quiz_id, :question_text, :option_a, :option_b, :option_c, :option_d, :correct_option)");
+        $statement->execute(array(":quiz_id" => $quiz_id, ":question_text" => $question_text, ":option_a" => $option_a, ":option_b" => $option_b, ":option_c" => $option_c, ":option_d" => $option_d, ":correct_option" => $correct_option));
+        return $statement->rowCount();
+    }
+
+    public function obtener_pregunta ($question_id) {
+        $statement = $this->conn->prepare("SELECT * FROM question WHERE question_id = :question_id");
+        $statement->execute(array(":question_id" => $question_id));
+        return $statement->fetchObject("Question");
+    }
+
+    public function obtener_preguntas_quiz ($quiz_id): array {
+        $statement = $this->conn->prepare("SELECT * FROM question WHERE quiz_id = :quiz_id");
+        $statement->execute(array(":quiz_id" => $quiz_id));
+        return $statement->fetchAll(PDO::FETCH_CLASS, "Question");
+    }
+
+    public function update_question ($question_id, $question_text, $option_a, $option_b, $option_c, $option_d, $correct_option): int {
+        $statement = $this->conn->prepare("UPDATE question SET question_text = :question_text, option_a = :option_a, option_b = :option_b, option_c = :option_c, option_d = :option_d, correct_option = :correct_option WHERE question_id = :question_id");
+        $statement->execute(array(":question_id" => $question_id, ":question_text" => $question_text, ":option_a" => $option_a, ":option_b" => $option_b, ":option_c" => $option_c, ":option_d" => $option_d, ":correct_option" => $correct_option));
+        return $statement->rowCount();
+    }
+
+    public function borrar_question ($question_id): int {
+        $statement = $this->conn->prepare("DELETE FROM question WHERE question_id = :question_id");
+        $statement->execute(array(":question_id" => $question_id));
         return $statement->rowCount();
     }
 }
