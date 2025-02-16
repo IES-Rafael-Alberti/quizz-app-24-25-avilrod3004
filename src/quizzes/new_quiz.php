9a22068ceb0799a45e6340fb8b988ea048c7fd61
avilrod3004 <avilrod3004@g.educaand.es>
2025-02-16 20:35:11 +0100

    maquetacion formulario para crear nuevos quizzes

--- src/quizzes/new_quiz.php
+++ src/quizzes/new_quiz.php
@@ -12,18 +12,32 @@ if (!isset($_SESSION["username"])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href=../css/reset.css>
+    <link rel="stylesheet" href="../css/variables.css">
+    <link rel="stylesheet" type="text/css" href="../css/header.css">
+    <link rel="stylesheet" type="text/css" href="../css/body.css">
     <title>Nuevo quiz</title>
 </head>
-<body>
-    <main>
-        <form action="create_quiz.php" method="post">
+<body class="contenedor-formulario">
+    <header class="encabezado">
+        <img src="../img/quizzes!.png" alt="Quizzes App" class="encabezado__logo">
+        <nav class="encabezado__navegacion">
+            <ul class="navegacion__listado">
+                <li class="listado__opcion"><a href="../user/logout.php" class="opcion__enlace">Cerrar sesión</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="principal-formulario">
+        <h1 class="principal__titulo">Nuevo quiz</h1>
+        <form action="create_quiz.php" method="post" class="principal-formulario__formulario">
             <label for="title">Titulo</label>
             <input type="text" id="title" name="title">
 
             <label for="description">Descripción</label>
             <textarea name="description" id="description" cols="30" rows="5"></textarea>
 
-            <button type="submit">Registrar quiz</button>
+            <button type="submit" class="boton">Registrar quiz</button>
         </form>
 
         <?php
@@ -32,5 +46,9 @@ if (!isset($_SESSION["username"])) {
         }
         ?>
     </main>
+
+    <footer class="pie">
+        <p>soy el footer</p>
+    </footer>
 </body>
 </html>
