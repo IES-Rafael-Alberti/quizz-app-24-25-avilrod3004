27589fb838f9906aaafb3ca9441890129a9b2b20
avilrod3004 <avilrod3004@g.educaand.es>
2025-02-17 07:44:14 +0100

    maquetacion formulario para añadir preguntas a los quizzes

--- src/questions/add_question.php
+++ src/questions/add_question.php
@@ -18,45 +18,64 @@ $quiz_id = $_POST["quiz_id"];
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Nuevo quiz</title>
+    <link rel="stylesheet" href=../css/reset.css>
+    <link rel="stylesheet" href="../css/variables.css">
+    <link rel="stylesheet" type="text/css" href="../css/header.css">
+    <link rel="stylesheet" type="text/css" href="../css/body.css">
+    <title>Nueva pregunta</title>
 </head>
 <body>
-<main>
-    <form action="create_question.php" method="post">
-        <label for="quiz_id"></label>
-        <input type="text" name="quiz_id" id="quiz_id" hidden="hidden" value="<?= $quiz_id ?>">
+    <header class="encabezado">
+        <img src="../img/quizzes!.png" alt="Quizzes App" class="encabezado__logo">
+        <nav class="encabezado__navegacion">
+            <ul class="navegacion__listado">
+                <li class="listado__opcion"><a href="../user/logout.php" class="opcion__enlace">Cerrar sesión</a></li>
+            </ul>
+        </nav>
+    </header>
 
-        <label for="question_text">Pregunta</label>
-        <input type="text" id="question_text" name="question_text">
+    <main class="principal-formulario">
+        <h1 class="principal__titulo">Nueva pregunta</h1>
 
-        <label for="option_a">Opción A</label>
-        <input type="text" id="option_a" name="option_a">
+        <form action="create_question.php" method="post" class="principal-formulario__formulario">
+            <label for="quiz_id"></label>
+            <input type="text" name="quiz_id" id="quiz_id" hidden="hidden" value="<?= $quiz_id ?>">
 
-        <label for="option_b">Opción B</label>
-        <input type="text" id="option_b" name="option_b">
+            <label for="question_text">Pregunta</label>
+            <input type="text" id="question_text" name="question_text">
 
-        <label for="option_c">Opción C</label>
-        <input type="text" id="option_c" name="option_c">
+            <label for="option_a">Opción A</label>
+            <input type="text" id="option_a" name="option_a">
 
-        <label for="option_d">Opción D</label>
-        <input type="text" id="option_d" name="option_d">
+            <label for="option_b">Opción B</label>
+            <input type="text" id="option_b" name="option_b">
 
-        <label for="correct_option">Opción correcta</label>
-        <select name="correct_option" id="correct_option">
-            <option value="a">A</option>
-            <option value="b">B</option>
-            <option value="c">C</option>
-            <option value="d">D</option>
-        </select>
+            <label for="option_c">Opción C</label>
+            <input type="text" id="option_c" name="option_c">
 
-        <button type="submit">Registrar quiz</button>
-    </form>
+            <label for="option_d">Opción D</label>
+            <input type="text" id="option_d" name="option_d">
 
-    <?php
-    if (isset($_GET["error"])) {
-        echo '<span class="error">' . $_GET["error"] . "</span>";
-    }
-    ?>
-</main>
+            <label for="correct_option">Opción correcta</label>
+            <select name="correct_option" id="correct_option" class="formulario__correct_option">
+                <option value="a">A</option>
+                <option value="b">B</option>
+                <option value="c">C</option>
+                <option value="d">D</option>
+            </select>
+
+            <button type="submit" class="boton">Registrar quiz</button>
+        </form>
+
+        <?php
+        if (isset($_GET["error"])) {
+            echo '<span class="error">' . $_GET["error"] . "</span>";
+        }
+        ?>
+    </main>
+
+    <footer class="pie">
+        <p>soy el footer</p>
+    </footer>
 </body>
 </html>
