8221d426ce2ed752801cd46c9f7ab8ad785766e1
avilrod3004 <avilrod3004@g.educaand.es>
2025-02-17 09:36:24 +0100

    maquetacion pagina de inicio

--- src/index.php
+++ src/index.php
@@ -5,20 +5,21 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/variables.css">
-    <link rel="stylesheet" type="text/css" href="css/header.css">
+    <link rel="stylesheet" href=./css/reset.css>
+    <link rel="stylesheet" href="./css/variables.css">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" type="text/css" href="./css/body.css">
     <title>Quizzes App</title>
 </head>
-<body>
-    <header class="encabezado">
-        <h1 class="encabezado__titulo">Quizzes App</h1>
-
+<body class="contenedor-inicio">
+    <main class="inicio">
+        <img src="./img/quizzes!.png" alt="Quizzes App" class="inicio__imagen">
         <nav class="encabezado__navegacion">
             <ul class="navegacion__listado">
-                <li class="listado__opcion"><a href="user/login.php" class="opcion__enlace">Login</a></li>
-                <li class="listado__opcion"><a href="user/register.php" class="opcion__enlace">Registro</a></li>
+                <li class="listado__opcion"><a href="user/login.php" class="opcion__enlace">Iniciar sesión</a></li>
+                <li class="listado__opcion"><a href="user/register.php" class="opcion__enlace">Crear cuenta</a></li>
             </ul>
         </nav>
-    </header>
+    </main>
 </body>
 </html>
